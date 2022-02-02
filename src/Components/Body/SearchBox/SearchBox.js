0cb2a0de8fbf7582073b299b3bcbf6d31738509f
Daniel Ginting <daniel_yjg@yahoo.com>
2022-02-02 10:30:46 +0700

    searchbox border

--- src/Components/Body/SearchBox/SearchBox.js
+++ src/Components/Body/SearchBox/SearchBox.js
@@ -3,7 +3,7 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className='mt4 ml4 tl'>
             <input 
-                className=" pa3 "
+                className=" pa3 border-4"
                 type='search' placeholder="Search for a country..."
                 onChange={searchChange}
                 />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
-    "./src/Components/Body/DetailPage/DetailPage.js"
+    "./src/Components/Body/DetailPage/DetailPage.js",
+    "./src/Components/Body/SearchBox/SearchBox.js"
   ],
   theme: {
     extend: {},
